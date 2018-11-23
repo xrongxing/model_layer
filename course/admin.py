9ddb49a9b4cc9b96d1d4121ab56aea1774b220ad
Sam <sam@example.com>
2018-11-23 18:14:49 +0800

    commit model_layer

---
+++ course/admin.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+from .models import Blog, Author, Entry
+
+# Register your models here.
+
+admin.site.register(Blog)
+admin.site.register(Author)
+admin.site.register(Entry)
--- model_layer/settings.py
+++ model_layer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'opdlg$n8$=0z93$a5_gke$0mvfx%8uk5jz07iv#_c7y@7mz=p*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'course',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+#TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
